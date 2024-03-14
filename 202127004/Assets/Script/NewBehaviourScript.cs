9fcc18aae2a7a6b375699224ecc7dfe97bcca47d
Parkdabin13 <go7890dmdho@naver.com>
2024-03-14 12:01:06 +0900

    [GT]LinkedList1

--- 202127004/Assets/Script/NewBehaviourScript.cs
+++ 202127004/Assets/Script/NewBehaviourScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -7,7 +8,11 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int[] value = { 3, 5 };
+        MyLinkedList<int> list = new();
+        list.AddFirst(1);
+        list.AddLast(0);
+        list.Print();
     }
 
     // Update is called once per frame
@@ -17,28 +22,83 @@ public class NewBehaviourScript : MonoBehaviour
     }
 }
 
-public class MyQueue<T>
+public class MyNode<T>
 {
-    public T[] array;
-    public int size;
+    private T value;
+    private MyNode<T> next;
 
-    public MyQueue()
+    public T Value { get => value; set => this.value = value; }
+    public MyNode<T> Next { get => next; set => next = value; }
+    
+    public MyNode(T value, MyNode<T> next)
     {
-        size = 0;
-        array = new T[10];
+        this.value = value;
+        this.next = next;
+    }
+}
+
+public class MyLinkedList<T>
+{
+    private MyNode<T> first;
+    private int count;
+
+    public int Count { get => count; }
+    public MyNode<T> First { get => first; }
+
+    public MyLinkedList()
+    {
+        count = 0;
+        first = null;
     }
 
-    public MyQueue(T[] array)
+    public MyLinkedList(T[] values)
     {
-        size = array.Length;
-        this.array = array;
+        count = values.Length;
+        MyNode<T> prevNode = null;
+        for (int i = 0; i < count; i++) 
+        {
+            MyNode<T> node = new(values[i], null);
+            if (prevNode == null)
+            {
+                first = node;
+            }
+            else
+            {
+                prevNode.Next = node;
+            }
+            prevNode = node;
+        }
     }
 
-    public MyQueue(int count)
+    public MyNode<T> AddFirst(T value)
     {
-        size = 0;
-        array = new T[count];
+        count++;
+        return first = new MyNode<T>(value, count != 1 ? first : null);
     }
 
-    public void Enqueue() { }
+    public MyNode<T> AddLast(T value)
+    {
+        count++;
+        if (count == 1)
+        {
+            return first = new MyNode<T>(value, null);
+        }
+        MyNode<T> node = first;
+        while (node.Next != null)
+        {
+            node = node.Next;
+        }
+        MyNode<T> lastNode = new(value, null);
+        node.Next = lastNode;
+        return lastNode;
+    }
+    public void Print()
+    {
+        MyNode<T> node = first;
+        while (node != null)
+        {
+            Debug.Log(node.Value);
+            node = node.Next;
+        }
+    }
 }
